2429c9541edac0a24bfb4abc0a246a27b9090aea
stachoz <stasiekzdybel9@gmail.com>
2024-10-28 10:10:09 +0100

    draft of functions (with website urls) to scrapp words for each category

--- src/WordsListScrapper.py
+++ src/WordsListScrapper.py
@@ -1,3 +1,5 @@
+from warnings import deprecated
+
 import requests
 from bs4 import BeautifulSoup
 from requests import Response
@@ -20,6 +22,7 @@ class WebPageFetcher:
 
         return []
 
+    @deprecated
     def scrapp_words(self, response: Response):
         soup = BeautifulSoup(response.text, 'html.parser')
         words = set()
@@ -29,3 +32,54 @@ class WebPageFetcher:
             if(a_tag and len(a_tag.text) > 3):
                 words.add(a_tag.text)
         return list(words)
+
+    def scrapp_fruits(self):
+        url = "https://dietetycy.org.pl/owoce/" # 50
+        pass
+
+    def scrapp_vegetables(self):
+        url = "https://dietetycy.org.pl/warzywa/" # 50
+        pass
+
+    def scrapp_animals(self):
+        url = "https://all4mom.pl/lista-50-najpopularniejszych-zwierzat-domowych-oryginalni-pupile-dla-dzieci/" # 50
+        pass
+
+    def scrapp_colors(self):
+        url = "https://preply.com/en/blog/red-is-the-new-black-let-s-learn-colors-and-shades-in-english/" # 25
+        pass
+
+    def scrapp_places_in_town(self):
+        url = "http://www.grammar-monster.com/vocabulary/ESL-vocabulary-places-in-town.htm" # 12
+        pass
+
+    def scrapp_jobs(self):
+        url = "https://chatschool.pl/blog/zawody-po-angielsku/"  # 70
+        pass
+
+    def scrapp_hobbies(self):
+        url = "https://langeek.co/en/vocab/subcategory/800/word-list" # 24
+        pass
+
+    def scrapp_drinks(self):
+        url = "https://langeek.co/en/vocab/subcategory/146/word-list" # 28
+        pass
+
+    def scarpp_parts_of_house(self):
+        url = "https://www.woodwardenglish.com/lesson/parts-of-the-house/" # 22
+        pass
+
+    def scrapp_sports_words(self):
+        # TODO clean words from text inside ()
+        url = "https://www.britannica.com/dictionary/eb/3000-words/topic/american-sports-vocabulary-english" # 33
+        pass
+
+    def scrapp_music_instruments(self):
+        url = "https://promova.com/english-vocabulary/music-instruments-in-english" # 50
+        pass
+
+    def scrapp_technology(self):
+        url = "https://www.learnenglish.com/vocabulary/technology/learn-technology-vocabulary-in-english/" # 100
+        pass
+
+    
