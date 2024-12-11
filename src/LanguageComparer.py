cc783045f66b0f9b0ebadbd0128219fab7d106a6
KacperWawrylo <k.wawrylo@student.uksw.edu.pl>
2024-12-11 20:40:24 +0100

    Refactoring: moving data loading and adding results

--- src/LanguageComparer.py
+++ src/LanguageComparer.py
@@ -2,22 +2,18 @@ import csv
 
 
 class LanguageComparer:
-    def __init__(self, filename, num_translations):
-        # Inicjalizacja klasy i wczytanie danych z pliku CSV
+    def __init__(self, filename):
         self.filename = filename
-        self.num_translations = num_translations
-        # Dodaj w __init__():
         self.language_names = ['Angielski', 'Francuski', 'Wloski', 'Hiszpanski', 'Niemiecki', 'Polski', 'Czeski']
+        self.num_translations = len(self.language_names)
         self.data = []
         self.categories = {}
-        self.load_data()
 
     def load_data(self):
         with open(self.filename, 'r', encoding='utf-8') as csvfile:
             reader = csv.reader(csvfile)
-            headers = next(reader)
+            next(reader)
             for row in reader:
-                # Zakladam ze 7 pierwszych kolumn to tlumaczenia
                 translations = row[:self.num_translations]
                 category = row[self.num_translations]
                 self.data.append((translations, category))
@@ -27,48 +23,38 @@ class LanguageComparer:
 
     # Implementacja algorytmu Levenshteina
     def levenshtein_distance(self, s1, s2):
-        # Jezeli jedno ze slow jest puste, odlegglosc to dlugosc drugiego slowa
         if len(s1) == 0:
             return len(s2)
         if len(s2) == 0:
             return len(s1)
 
-        # Inicjalizujemy macierz odleglosci
         rows = len(s1) + 1
         cols = len(s2) + 1
-        # Tworzymy macierz o wymiarach (len(s1)+1) x (len(s2)+1)
         distance_matrix = [[0 for x in range(cols)] for x in range(rows)]
 
-        # Ustawiamy wartosci poczatkowe dla pierwszego wiersza i kolumny
         for i in range(1, rows):
-            distance_matrix[i][0] = i  # Koszt usunięcia wszystkich znaków z s1
+            distance_matrix[i][0] = i
         for j in range(1, cols):
-            distance_matrix[0][j] = j  # Koszt wstawienia wszystkich znaków z s2
+            distance_matrix[0][j] = j
 
-        # Przechodzimy przez macierz i obliczamy koszty
         for i in range(1, rows):
             for j in range(1, cols):
-                # Jezeli znaki sa takie same, koszt to 0
                 if s1[i - 1] == s2[j - 1]:
                     cost = 0
                 else:
-                    # Jezeli znaki są rozne, koszt to 1
                     cost = 1
-                # Obliczamy minimalny koszt operacji
                 distance_matrix[i][j] = min(
-                    distance_matrix[i - 1][j] + 1,  # Usuniecie`
-                    distance_matrix[i][j - 1] + 1,  # Wstawienie
-                    distance_matrix[i - 1][j - 1] + cost  # Zamiana
+                    distance_matrix[i - 1][j] + 1,
+                    distance_matrix[i][j - 1] + 1,
+                    distance_matrix[i - 1][j - 1] + cost
                 )
 
-        # Odleglosc Levenshteina to wartosc w prawym dolnym rogu macierzy
         return distance_matrix[rows - 1][cols - 1]
 
-    # Obliczenie srednich odleglosci Levenshteina dla kazdej pary jezykow w kazdej kategorii
     def compute_average_distances(self):
+        self.load_data()
         results = {}
         for category, translations_list in self.categories.items():
-            # Inicjalizacja struktur danych
             distances = {}
             counts = {}
             for i in range(self.num_translations):
@@ -77,7 +63,6 @@ class LanguageComparer:
                     distances[key] = 0
                     counts[key] = 0
 
-            # Obliczanie odleglosci dla kazdej pary tlumaczen
             for translations in translations_list:
                 for i in range(self.num_translations):
                     for j in range(i + 1, self.num_translations):
@@ -86,7 +71,6 @@ class LanguageComparer:
                         distances[key] += dist
                         counts[key] += 1
 
-            # Obliczanie sredniej
             average_distances = {}
             for key in distances:
                 if counts[key] > 0:
@@ -99,11 +83,9 @@ class LanguageComparer:
 
         return results
 
-    # Zapisanie wyników do pliku CSV
     def save_results_to_csv(self, results, output_filename):
         with open(output_filename, 'w', newline='', encoding='utf-8') as csvfile:
             writer = csv.writer(csvfile)
-            # Nagłówki kolumn
             header = ['Kategoria', 'Język 1', 'Język 2', 'Średnia odległość Levenshteina']
             writer.writerow(header)
             for category, averages in results.items():
