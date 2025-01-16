49e94bdf8fdf12d4d5283430494e1f10f09e80d3
Marrey12 <96487341+Marrey12@users.noreply.github.com>
2025-01-16 20:59:24 +0100

    Update WordsSimilarityLogic.py
    Customizing code to make entire project work

--- src/WordsSimilarityLogic.py
+++ src/WordsSimilarityLogic.py
@@ -6,17 +6,7 @@ from src.WordsSimilarityGraph import WordsSimilarityGraph
 
 class WordsSimilarityLogic:
 
-     def run(self):
-        graph_generator = WordsSimilarityGraph("data/levenshtein_results.csv")
-
-        
-        print("Generating graphs for all categories...")
-        graph_generator.generate_category_graphs()  
-
-        
-        print("Generating graph for average distances")
-        average_graph = graph_generator.create_average_graph()  
-        graph_generator.save_graph(average_graph, "average_distances")  
+  
 
     scraper_output_file = "words_data.csv"
     translator_output_file = "translated_words.csv"
@@ -35,3 +25,13 @@ class WordsSimilarityLogic:
         language_comparer = LanguageComparer(self.translator_output_file)
         result = language_comparer.compute_average_distances()
         language_comparer.save_results_to_csv("levenshtein.csv", result)
+         
+          graph_generator = WordsSimilarityGraph("data/levenshtein_results.csv")
+
+       
+       
+        graph_generator.generate_category_graphs()  # Generowanie grafów dla wszystkich kategorii
+
+        
+        average_graph = graph_generator.create_average_graph()  # Tworzenie wykresu dla średnich odległości
+        graph_generator.save_graph(average_graph, "average_distances")  # Zapisanie wykresu
